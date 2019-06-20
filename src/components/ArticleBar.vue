dd32a41cb97867c1cf558178d59e525505bbb2a8
Marichka Offen <marichka.offen@gmail.com>
2019-06-20 02:53:52 -0400

    Create artticle navigation and reg navbar

---
+++ src/components/ArticleBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="nav">
+    <h4 class="title">History Articles</h4>
+    <router-link :to="{ name: 'kent-militia' }">Kent Militia</router-link>
+    <router-link :to="{ name: 'czechoslovak-legion' }"
+      >Czechoslovak Legion</router-link
+    >
+    <router-link :to="{ name: 'irish-history' }">Irish History</router-link>
+    <router-link :to="{ name: 'dutch-history' }">Dutch History</router-link>
+    <router-link :to="{ name: 'scottish-history' }"
+      >Scottish History</router-link
+    >
+    <router-link :to="{ name: 'colonial-history' }"
+      >Colonial History</router-link
+    >
+    <router-link :to="{ name: 'hussites' }">Hussites</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  margin: 0;
+  border-bottom: 1.5px solid rgba(#8d6f6f, 0.6);
+  color: #a72525;
+}
+
+#nav {
+  display: flex;
+  flex-direction: column;
+  width: 15rem;
+  padding: 1.5rem;
+  text-align: right;
+  border-right: 1.5px solid rgba(#8d6f6f, 0.6);
+}
+
+a {
+  color: #444;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="nav">
+    <router-link :to="{ name: 'home' }">Home</router-link>
+    <router-link :to="{ name: 'about' }">About</router-link>
+    <router-link :to="{ name: 'book' }">My Book</router-link>
+    <router-link :to="{ name: 'recommendations' }">Books I Recommend</router-link>
+    <router-link :to="{ name: 'contact' }">Contact Me</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#nav {
+  display: flex;
+  flex-direction: column;
+  width: 15rem;
+  padding: 1.5rem;
+  text-align: right;
+  border-right: 1.5px solid rgba(#8d6f6f, 0.6);
+}
+a {
+  color: #444;
+}
+</style>
