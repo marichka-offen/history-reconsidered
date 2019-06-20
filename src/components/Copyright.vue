9881a97ffe5543ba28a959f5ccb3c2a2bd943238
Marichka Offen <marichka.offen@gmail.com>
2019-06-20 02:54:19 -0400

    Create quote and copyright components

---
+++ src/components/Copyright.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <small>Copyright © 2010 History Reconsiderered, LLC</small>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/MainQuote.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p class="winston">
+      "The further backward you look, the further forward you can see" –
+      W.Churchill
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.winston {
+  font-family: "Windsong", cursive;
+  text-align: center;
+  font-size: 4rem;
+  margin: 0;
+}
+</style>
