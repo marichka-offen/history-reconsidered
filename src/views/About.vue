a29b11084c55f96b3220344a1b153d2acce80fb8
Marichka Offen <marichka.offen@gmail.com>
2019-06-20 02:54:52 -0400

    Create about view

---
+++ src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>About History Reconsidered</h2>
+    <div class="container">
+      <img src="#" alt="Lee Offen" />
+
+      <p>
+        HistoryReconsidered.net was conceived and written by Lee Offen.
+        <br />
+        <br />Lee is a retired U.S. Army Major who resides in Springfield
+        Virginia. He is an Armored Cavalry Combat Veteran, former Airborne
+        Infantryman and a recipient of the Bronze Star. He has his Masters
+        Degree in Government from Campbell University and was Assistant
+        Professor of Military Science at Georgetown University and George Mason
+        University. A graduate of the U.S. Army Military History Instructors
+        Course, he is also a historical re-enactor and avid student of 17th and
+        18th century history.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+img {
+  border: 1px solid black;
+  display: inline-block;
+  width: 35rem;
+  height: 40rem;
+  float: left;
+  margin-right: 2rem;
+}
+</style>
