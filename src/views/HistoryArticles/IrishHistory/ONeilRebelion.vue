a79be41e342bb9bc9d3589758332fded03598f72
Marichka Offen <marichka.offen@gmail.com>
2019-06-21 23:38:33 -0400

    Create Oneal Rabelion  view

---
+++ src/views/HistoryArticles/IrishHistory/ONeilRebelion.vue
@@ -0,0 +1,294 @@
+<template>
+  <div>
+    <h2>O'Neil’s Rebellion The Nine Years' War 1593-1603</h2>
+    <p>
+      Up until 1594 Hugh O'Neill, "The O'Neill", or, Earl of Tyrone, as he was
+      known to the English, was pledged to the support of Elizabeth I Queen of
+      England. The degree to which he took that commitment seriously can be
+      measured by the fact that he took to the field with his brother in law,
+      the English Marshall Bagnel, against Hugh Maguire, who was in rebellion
+      against the English. This joint force defeated Maguire in 1593 at the
+      battle known as "The Passage of the Erne at Belleek". After this
+      engagement, O'Neill withdrew his support for the English, expressing
+      several grievances to commissioners sent by Elizabeth I. One of his
+      complaints was the absence of proper recognition for his support for the
+      English Crown. The replacement of the Viceroy and a limit upon actions
+      that could be taken against him, by his estranged English brother in law
+      Bagenal among others, indicate that Elizabeth had considered his
+      grievances favorably. The English were left with the impression that
+      O'Neill was still loyal to the English Crown.
+      <br />
+      <br />In June of 1594, a soon to be ally of O'Neill, Hugh O'Donnell,
+      ambushed and defeated an English relief column on the road to Ennniskillen
+      at in a battle known as 'The Ford of the Biscuits". O'Neill was not
+      present, but his brother Cormac was, along with 300 musketeers and 100
+      horse. In February of 1595 his other brother, Art MacBaron, participated
+      in the successful attack on an English fort at the ford of the Black
+      River. It remains to be determined if O'Neill was supporting his brothers,
+      as he remained aloof from the actions taken against the English.
+      <br />
+      <br />With the introduction of a new Viceroy, the English stepped up their
+      efforts to end the on again off again resistance they were experience in
+      their efforts to establish and supply English outposts on the edge of the
+      Pale. The English increased the number of forces in Ireland. True to form,
+      each of the three separate English armies were commanded by independent
+      commanders unable or unwilling to support each other.
+      <br />
+      <br />O'Neill allied himself with Hugh O'Donnell, whose power base lay in
+      Connaught and who was already out in rebellion. Tyrone's active role
+      started with the investment of Monaghan Castle. O'Neill's brother in law,
+      Bagenal, was able to supply the garrison of the castle, but was forced to
+      contend with ambushes along his route of march. As Bagenal was leaving, en
+      route to Newry, he was met by O'Neill at Clontibret who fought the column
+      to a standstill with ambushes and harassing fire until sunset. The Irish
+      demonstrated no attachment to castle or town and were perfectly willing to
+      raze either in order to retain operational mobility and not tie down
+      forces to defend them from the English. An English raid that destroyed
+      Dungannon in August 1595, O'Neill's own castle had no effect on the Irish.
+      <br />
+      <br />In September of 1595 O'Neill sued for peace and pardon. The English
+      at the time were at their most vulnerable but O'Neill may have had some
+      appreciation for the inability of the Irish to defeat the English
+      militarily and the fragile nature of his alliance. He used 1596 and 1597
+      to stall the English, increase his efforts to get Spanish military support
+      and to convince Irish leaders to join him or replace those who would not.
+      <br />
+      <br />O'Neill was executing a foreign policy of sorts in that he was
+      appealing to Catholic Spain for aid against the Protestant English, using
+      the restoration of the Catholic Church in Ireland as a basis for the
+      request for troops and equipment. Despite his best efforts, Scots
+      mercenaries were not forthcoming, but weapons were, purchased in England
+      and Scotland.
+      <br />
+      <br />O'Neill had also begun a considerable modernization effort that
+      continued throughout his rebellion, converting Kern to musket and caliver
+      formations and converting some Gallowglas to pike formations as well as
+      providing the requisite training and fielding the equipment necessary for
+      the transition. Throughout this modernization initiative, O'Neill retained
+      the flexibility to fight his army in the more traditional manner of
+      fighting the English in Ireland, using hit and run tactics and ambushes
+      against English formations moving across the Irish countryside. His
+      modernization effort made it possible for him to at least retain the
+      option of meeting the English on the field as equals, given enough time
+      and training.
+      <br />
+      <br />The interlude of peace ended in July 1597. The new Viceroy, one Lord
+      Brough, was on the offensive again and was met by O'Neill at the ford over
+      the Blackwater River. He overcame the 40 Irish defenders in a small
+      sconce, or fort, there with 1,000 men and proceeded to occupy the fort to
+      cover the approaches to the ford. Brough lacked the forces or the will to
+      carry the fight into Ulster and so remained in place. Bagenal conducted a
+      raid into Ulster burning Dungannon but accomplishing little else.
+      <br />
+      <br />At Christmas of 1597 O'Neill met with Elizabeth's commissioners and
+      in return for peace demanded a pardon, return of property of for his
+      followers Liberty of Conscience, withdrawal of all English garrisons and
+      Palatinate status for his holdings. Had the English agreed to that, it
+      would have established a separate and independent Irish Kingdom with
+      O'Neill as its ruler. The English did not agree to his terms and so both
+      sides waited for summer to renew hostilities.
+      <br />
+      <br />By the summer of 1598, O'Neill was prepared to move again, invested
+      the Blackwater Fort and waited for the English to come to the garrison's
+      rescue. O'Neill met his former brother in law and old nemesis at the
+      Yellow Ford. Bagenal's route of march went from Armagh on a narrow road
+      through the Yellow Ford and from there to the fort on the Blackwater
+      River. The engagement began when Bagenal's lead element reached a trench
+      dug by the Irish across his route of march. Simultaneous attacks occurred
+      on the lead element and trail elements with the center element raked by
+      fire along its flanks. The English army disintegrated and fled the field
+      after approximately an hour and a half with ¾ of the army dead or missing
+      and Bagenal dead. The town of Parma was returned to O'Neill and the
+      English abandoned the fort at Black Water.
+      <br />
+      <br />O'Neill's victory caused the Irish in all four provinces to rise up
+      against the English. Even Munster, which had been put to fire and sword
+      after the earlier Desmond uprising, was no longer safe for Englishmen.
+      English refugees began gathering in walled cities and fleeing Ireland for
+      England. Congratulations, as well as the possibility of support, came from
+      Spain and the Pope in Rome. O'Neill began a "scorched earth" effort in
+      preparation for an English attempt to retake previously held areas of
+      Ireland.
+      <br />
+      <br />Elizabeth I accurately assessed the challenge England was facing and
+      sent the largest force to date under the command of the Earl of Essex to
+      put down O'Neill's uprising and to pacify Ireland. Essex moved south into
+      Munster depleting his force by garrisoning towns along his route of march
+      and failing to adequately feed his sick and starving army made up of
+      inexperienced English conscripts. Essex did fight a brief engagement at
+      the Pass of the Plumes in which he remained on the field of battle as the
+      Irish fled and took Cahir Castle. Elizabeth grew impatient at the cost and
+      lack of results of his campaign. When Essex finally turned north to fight
+      O'Neill, he met him face to face, established a truce and departed for
+      England never to return. O'Neill attempted to negotiate a peace from the
+      truce asking England to recognize, in effect, a Catholic Kingdom of
+      Ireland, with him as its ruler. Once again, the English refused to agree
+      to such terms.
+      <br />
+      <br />In the spring of 1600 O'Neill faced Lord Montjoy who, by
+      establishing a base of operations in the north west corner of Ireland
+      under the control of Sir Henry Dowcra as well as sending Lord Carew south
+      into Munster, began to threaten the alliance upon which the rebellion was
+      based. Some of O'Neills more faint of heart followers began defecting to
+      the English which led to an encirclement and an ever shrinking perimeter
+      of land and towns he controlled in Tyrone. Montjoy, unlike his
+      predecessors kept his forces in the field year round putting O'Neill under
+      considerably more pressure than he was accustomed to. He also insured the
+      harvest of 1600 did not take place, nor the planting of 1601.
+      <br />
+      <br />In the summer of 1600 a Spanish force landed on the southwest corner
+      of Ireland at Kinsale. It is only possible to speculate as to why they
+      landed so far away from Tyrone. Among the possible reasons were efforts by
+      rebel leaders in Munster to have their position bolstered by the Spanish
+      presence or that Kinsale was seen as a staging area for an invasion of
+      England. In either case, O'Neill had to choose whether or when to leave
+      his base of operations in Tyrone to move south to support the Spanish
+      force. This was a difficult decision due to the fragile nature of his
+      alliance making it likely that he might lose Tyrone upon his departure.
+      <br />
+      <br />Montjoy moved on Kinsale and invested it with little difficulty due
+      to the Spanish failure to move out of Kinsale into the surrounding
+      countryside. As Montjoy was surrounding Kinsale, O'Neill conducted several
+      raids into the Pale probably in an effort to force Montjoy or Carew to
+      focus on him and lift the siege of Kinsale. Montjoy refused to be
+      distracted and let the Pale burn. By November, O'Neill and his long term
+      ally O'Donnell, began to move south in two columns to relieve the Spanish.
+      Both columns managed to outmaneuver an effort by Carew to impede their
+      progress south.
+      <br />
+      <br />On Christmas Eve, 1601, after consolidating all his forces in front
+      of Kinsale, O'Neill marched his forces onto the field in a three tercio
+      formation of musket and pike. The English, under leaders far more
+      experienced and soldiers more trained at this mode of traditional European
+      warfare were able to outmaneuver the Irish formations and routed them from
+      the field. The Spanish did not support the Irish with any attempt to
+      breakout of Kinsale permitting the English to bring the majority of their
+      forces to bear on O'Neill's forces in the battle and the follow on pursuit
+      of the broken Irish formations. O'Neill fled and made his way north back
+      to Tyrone.
+      <br />
+      <br />The battle of Kinsale in effect ended the rebellion even though
+      O'Neill spent a year in hiding before making his submission to the
+      English. During that year the English had taken control of all of Ireland.
+      O'Neill's submission was accepted on the 24th of March, 1603 the day after
+      Elizabeth I died. He remained in Ireland, stripped of his earldom and the
+      title of "The O'Neill" until 17 September, 1607 when he fled Ireland with
+      his fellow former Irish leaders, in an event called the "Flight of the
+      Earls." Those not of noble blood stayed behind to feel the full wrath of
+      the English, not to rise up again until 1636.
+      <br />
+      <br />Throughout the rebellion, the English were limited by terrain to
+      only three avenues of approach out of the Pale to invade Ulster and
+      Connaught. Initially the English sought to control the terrain by
+      establishing a series of outposts along these routes. All but one of the
+      major battles fought during the rebellion were fought on these routes. At
+      Clonbriet, Blackwater Ford and Yellow Ford the Irish forces positioned
+      themselves between an English garrison and a relieving or resupplying
+      force. This gave the Irish the ability to chose the terrain upon which
+      battles would take place, taking maximum advantage of their knowledge of
+      the countryside. These battles can be described best as "running battles",
+      with the Irish engaging English columns from the flanks and from prepared
+      positions to the column's front. As the English would respond, the Irish
+      would, if necessary to preserve combat power, withdraw in an orderly
+      fashion to another position. In this manner the English seeking to
+      resupply garrisons were forced to pay a high cost in manpower, lead and
+      powder, all of which were is short supply and only replaceable across long
+      supply lines reaching back to England. It was not until Montjoy bypassed
+      these routes into Ulster and Connaught by landing on the northwest coast,
+      sending Carew into Munster and keeping forces in the Pale that the English
+      were able to break the stalemate the Irish were able to use to their
+      advantage so successfully. Kinsale, the last battle of the uprising, found
+      O'Neill attempting to meet the English on an open field. It is very likely
+      that the battle was lost as a result of this first and only attempt to
+      meet the English forces on an open field on English terms.
+    </p>
+    <h3>A Brief Summary of Irish Military Organization in the 16th century</h3>
+    <p>
+      The highest office held by an Irishman outside those parts of Ireland
+      occupied by the English in the 16th century was the usually hereditary
+      Ceannfine or Chief of a Tuath (a tribal area occupying as much as 400
+      square miles). Below him was his Tanist or the heir to the Ceannfine. Next
+      in the hierarchy came the Urraigha, or sub-chiefs.
+      <br />
+      <br />The Urraigha were responsible for supporting the Ceannfine if called
+      upon, in a Gairmsluaigh ("calling out") with all free men who were not
+      Clergy, poets, harpers, physicians, scholars or lawyers. These were
+      Cernagh, a term anglicized to Kern. They were expected to report with all
+      weapons and three days worth of victuals. By mid 16th century those who
+      were not free were also pressed into service. These were part time
+      soldiers who augmented full time professional military men.
+      <br />
+      <br />The full time military in 16th century Ireland were called
+      Buannadha, a term Anglicized to Buannacht or billeted men. They were paid
+      "coyne and livery" which were their wages, or "coyne" and care for their
+      horses if they owned any, or "livery."
+      <br />
+      <br />In a Ceannfine or Urraigh household it was possible to find both
+      Cernagh and Galloglaich, a term anglicized to gallowglas, which means
+      foreign warrior. The Galloglaich were mercenaries from families originally
+      from the Western Isles and the western coast of Scotland that had been a
+      military presence in Ireland since the 12th century.
+      <br />
+      <br />The Cernagh were typically armed with knife, sword, bow or darts
+      transitioning over time to musket and caliver. They served as scouts and
+      skirmishers. The Cernagh also served as cavalry, riding stirrup-less
+      saddles and using lances in an overhand manner making them well suited for
+      reconnaissance but unable to meet English cavalry in a stand up fight. The
+      Galloglaich were typically armed with two-handed swords and or lochaber or
+      sparth axes with some serving in pike formations as the 16th century
+      progressed. There can be little doubt that there were no hard and fast
+      rules concerning armament of these two groups. Each pair of Cernagh were
+      entitled to a servant to help carry equipment. Each Galloglaich or mounted
+      Cernagh was entitled to a pair of servants or "horse boys" to help carry
+      equipment. The servants were armed with javelins or bows and expected to
+      join their masters in battle.
+      <br />
+      <br />As the 16th century progressed, there were numerous Cernagh who
+      became musketeers and calivermen while some of the Galloglaich migrated to
+      the small number of pike used by the Irish. Firearms were of more value
+      than pike in the hit and run tactics favored by the Irish against the
+      English. ONeill, during his rebellion against the English crown, fostered
+      this process of modernization.
+      <br />
+      <br />Another group appeared in Ireland in the late 16th century. They
+      were the "New Scots" who, unlike their predecessors chose not to settle,
+      but served in Irish armies and returned to Scotland. They were employed as
+      horse or foot.
+      <br />
+      <br />The buannadha were organized into corrughadh, called a company or
+      battle by the English and commanded by a consapal or captain. This was
+      further divided into spars in the case of the Galloglaich, which consisted
+      of one Galloglaich and two servants. A Galloglaich Corrughadh would
+      usually consist of 60 to 80 Galloglaich with the consapal permitted to
+      draw pay for an even 100; the difference being called "dead pays" and the
+      source of the consapal's pay. Pay for Galloglaich was set at one bullock
+      per quarter as pay and two for sustenance. The Cernagh received one heifer
+      per quarter and victuals.
+      <br />
+      <br />The importance of the cattle herd roaming Ireland called creaghts
+      cannot be over emphasized, as they provided the requisite augmentation to
+      the field rations of oatmeal and whiskey needed to keep the army combat
+      ready. Irish formations on the move would coordinate in advance a link up
+      with one of these creaghts and boil the meat in its own hide, saving the
+      army the need to carry cook pots.
+    </p>
+    <hr />
+    <h3>Sources:</h3>
+    <ol>
+      <li>"The Irish Wars" by Ian Heath and David Sque</li>
+      <li>"Elizabeth's Irish Wars" by Cyril Falls.</li>
+      <li>"Ireland in the Age of the Tudors, 1447-1603" by Steven G. Ellis.</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+ol {
+  font-size: 80%;
+  color: #444;
+}
+</style>
