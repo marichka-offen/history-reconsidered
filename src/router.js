3aa9ed556be961ad162b949e82c987c6f77b1d15
Marichka Offen <marichka.offen@gmail.com>
2019-06-21 23:35:12 -0400

    Create routes for multiple views

--- src/router.js
+++ src/router.js
@@ -7,12 +7,25 @@ import Recommend from "./views/Recommend.vue";
 import Contact from "./views/Contact.vue";
 
 // HISTORY ARTICLES ROUTES
+// KENT MILITIA
 import KentMilitia from "./views/HistoryArticles/KentMilitia/KentMilitia.vue";
+import Maryland from "./views/HistoryArticles/KentMilitia/Maryland.vue";
+// CZECHOSLVAK LEGION
 import CzechoslovakLegion from "./views/HistoryArticles/CzechoSlovakLegion/CzechoslovakLegion.vue";
+// IRISH HISTORY
 import IrishHistory from "./views/HistoryArticles/IrishHistory/IrishHistory.vue";
+import ONeilRebelion from "./views/HistoryArticles/IrishHistory/ONeilRebelion.vue";
+import ThreeKingdoms from "./views/HistoryArticles/IrishHistory/ThreeKingdoms.vue";
+import Ireland from "./views/HistoryArticles/IrishHistory/Ireland.vue";
+import WilliamiteWar from "./views/HistoryArticles/IrishHistory/WilliamiteWar.vue";
+import IrishMilEstabl from "./views/HistoryArticles/IrishHistory/IrishMilEstabl.vue";
+// DUTCH HISTORY
 import DutchHistory from "./views/HistoryArticles/DutchHistory/DutchHistory.vue";
+// SCOTTISH HISTORY
 import ScottishHistory from "./views/HistoryArticles/ScottishHistory/ScottishHistory.vue";
+// COLONIAL HISTORY
 import ColonialHistory from "./views/HistoryArticles/ColonialHistory/ColonialHistory.vue";
+// THE HUSSITES
 import Hussites from "./views/HistoryArticles/Hussites/Hussites.vue";
 
 Vue.use(Router);
@@ -52,6 +65,11 @@ export default new Router({
       component: KentMilitia
     },
     {
+      path: "/kent-militia/maryland",
+      name: "kent-militia-maryland",
+      component: Maryland
+    },
+    {
       path: "/czechoslovak-legion",
       name: "czechoslovak-legion",
       component: CzechoslovakLegion
@@ -60,6 +78,31 @@ export default new Router({
       path: "/irish-history",
       name: "irish-history",
       component: IrishHistory
+    },
+    {
+      path: "/irish-history/oneils-rebelion",
+      name: "oneils-rebelion",
+      component: ONeilRebelion
+    },
+    {
+      path: "/irish-history/three-kingdoms",
+      name: "three-kingdoms",
+      component: ThreeKingdoms
+    },
+    {
+      path: "/irish-history/ireland-1652-1688",
+      name: "ireland-1652-1688",
+      component: Ireland
+    },
+    {
+      path: "/irish-history/williamite-war",
+      name: "williamite-war",
+      component: WilliamiteWar
+    },
+    {
+      path: "/irish-history/irish-military",
+      name: "irish-military",
+      component: IrishMilEstabl
     },
     {
       path: "/dutch-history",
