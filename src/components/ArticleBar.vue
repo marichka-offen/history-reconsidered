94da5db9da2bbf1ef398f16dc531d78e5707615a
Marichka Offen <marichka.offen@gmail.com>
2019-06-21 23:35:34 -0400

    Create navbar for articles

--- src/components/ArticleBar.vue
+++ src/components/ArticleBar.vue
@@ -1,24 +1,67 @@
 <template>
   <div id="nav">
     <h4 class="title">History Articles</h4>
-    <router-link :to="{ name: 'kent-militia' }">Kent Militia</router-link>
+    <router-link :to="{ name: 'kent-militia' }">
+      <!-- KENT MILITIA -->
+      <span @click="kent = !kent">Kent Militia</span>
+    </router-link>
+    <router-link :to="{ name: 'kent-militia-maryland' }" v-show="!kent">
+      <small>Maryland (1634 - 1707)</small>
+    </router-link>
+
+    <!-- CZECHOSLVAK LEGION -->
     <router-link :to="{ name: 'czechoslovak-legion' }"
       >Czechoslovak Legion</router-link
     >
-    <router-link :to="{ name: 'irish-history' }">Irish History</router-link>
+
+    <!-- IRISH HISTORY -->
+    <router-link :to="{ name: 'irish-history' }">
+      <span @click="irish = !irish">Irish History</span>
+    </router-link>
+
+    <router-link :to="{ name: 'oneils-rebelion' }" v-show="!irish">
+      <small>O'Neil's Rebellion</small>
+    </router-link>
+    <router-link :to="{ name: 'three-kingdoms' }" v-show="!irish">
+      <small>War of Three Kingdoms</small>
+    </router-link>
+    <router-link :to="{ name: 'ireland-1652-1688' }" v-show="!irish">
+      <small>Ireland (1652-1688)</small>
+    </router-link>
+    <router-link :to="{ name: 'williamite-war' }" v-show="!irish">
+      <small>Williamite War</small>
+    </router-link>
+    <router-link :to="{ name: 'irish-military' }" v-show="!irish">
+      <small>Irish Military Establishment</small>
+    </router-link>
+
+    <!-- DUTCH HISTORY -->
     <router-link :to="{ name: 'dutch-history' }">Dutch History</router-link>
+
+    <!-- SCOTTISH HISTORY -->
     <router-link :to="{ name: 'scottish-history' }"
       >Scottish History</router-link
     >
+
+    <!-- COLONIAL HISTORY -->
     <router-link :to="{ name: 'colonial-history' }"
       >Colonial History</router-link
     >
+
+    <!-- THE HUSSITES -->
     <router-link :to="{ name: 'hussites' }">Hussites</router-link>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      kent: true,
+      irish: true
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
