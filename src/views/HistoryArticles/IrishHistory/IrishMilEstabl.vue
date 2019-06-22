874b267eb42a40dcbd9e9be6a4c084062d87b21d
Marichka Offen <marichka.offen@gmail.com>
2019-06-21 23:40:30 -0400

    Create Irish Military component

---
+++ src/views/HistoryArticles/IrishHistory/IrishMilEstabl.vue
@@ -0,0 +1,204 @@
+<template>
+  <div>
+    <h3>The Irish Military Establishment, 1660-1688</h3>
+    <p>
+      The post restoration Military Establishment of Ireland dates from February
+      1661 and consisted of thirty troops of independent horse organized into
+      six county based regiments (Lord Lieutenant’s, Munster, Connaught,
+      Ormond’s until 1662, Ossery’s and the Marshals Regiments of Horse) and
+      sixty six companies of independent foot, mostly under the command of
+      former Parliamentarians with lifetime commissions. This was found to be
+      inadequate, given the dispersed nature of the units and their lack of
+      direct responsibility to the Viceroy.
+    </p>
+    <blockquote>
+      "His majesty having thought fit to raise in England a Regiment of twelve
+      hundred foot to be his Guards in Ireland"
+    </blockquote>
+    <h6>– Charles II, 23 April 1662</h6>
+    <p>
+      With this royal warrant, the first foot regiment in the post restoration
+      Irish Military Establishment came into being.
+      <br />
+      <br />The King’s Regiment of Guards in the Kingdom of Ireland finds it
+      origins in a Regiment raised out of Connaught by George Cusack in 1653.
+      George Cusack held the garrison of Inishbofin against Parliament, with the
+      support of the Duke of Lorraine, until 1653. Cusack based on his
+      relationship with the Duke of Lorraine was able to take a regiment out of
+      Ireland into French Service at the conclusion of the siege. After a
+      Commonwealth-French Alliance was established in 1656, Cusack took his
+      regiment into Charles Stuart’s army in Spanish Flanders where it was
+      merged into the Marquis of Ormond’s Regiment, consisting of 700 hundred
+      men and commanded by Colonel George Grace. The regiment fought at the
+      Battle of the Dunes in 1658, an abortive Spanish attempt to relieve the
+      French-English siege of Dunkirk.
+      <br />
+      <br />Some of the men of the regiment and many of the officers were
+      transferred to England for service in Ireland in 1662. Those that remained
+      in the Spanish Netherlands were sent to Tangier. The Marquis, now First
+      Duke of Ormond, the Viceroy of Ireland while in exile, and now the Lord
+      Lieutenant of Ireland, recruited additional soldiers for the regiment in
+      England and granted additional commissions as he saw fit. The Regiment was
+      commanded by his son, the Earl of Arran, later the Second Duke of Ormond,
+      who led the Regiment until 1688. The rolls included a Colonel, Lieutenant
+      Colonel, a Major, nine Captains, twelve Lieutenants, twelve Ensigns, forty
+      sergeants, thirty six corporals, a drum major with twenty four drummers, a
+      piper in the Kings Company and twelve hundred men. The Regiment was
+      organized into twenty troops of horse and eight companies of foot.
+      Clothing and equipment was the same as Colonel Russell's Regiment of
+      Guards. One wonders if the cross on the "taffey" yellow background was the
+      Cross of St George or the Cross of St Patrick, used by the Viceroys of
+      Ireland.
+    </p>
+
+    <!-- PICTURE AND CITATION -->
+    <p>
+      The Regiment embarked for Dublin in May of 1662, where it was
+      headquartered while companies assumed garrison duty throughout Ireland.
+      The Viceroy had a personal bodyguard at the Castle in Dublin in the form
+      of the Company of Battleaxes, comprised of a captain, two sergeants and
+      sixty men. The unit existed from 1662 to 1665. A Troop of the King’s Guard
+      of Horse was created in 1662 that existed until 1684. The Regiment of
+      Guards had both a ceremonial function but were called upon from time to
+      time to quell disturbances and perform law enforcement functions
+      throughout Ireland. In 1663 a Plot to capture Dublin Castle and the Lord
+      Lieutenant led by Captain Blood and involving seventy “Presbyterians” was
+      foiled. In 1663 the Regiment of Guards was augmented by an additional two
+      hundred soldiers, some of whom were found to be unsuitable as they were
+      cashiered soldiers from Ireland. The officers that came with this
+      augmentation were, at the insistence of Ormond, Irish, and not English. By
+      1663 financing the military establishment of Ireland was already an issue
+      and there were no disbandment of units because the Crown could not afford
+      to pay the soldiers arrears in pay. In 1666, the garrison of Carrickfergus
+      mutinied taking control of the castle and the town. The Earl of Arran and
+      four companies of Guards sailed to Carrickfergus and put down the uprising
+      in two days. The one hundred and ten mutineers were court-martialed and
+      the two companies were disbanded.
+      <br />
+      <br />The Establishment was reorganized in 1672 with the five existing
+      regiments of Horse and the troop of Life Guards and five regiments of foot
+      (Massie’s, Howth’s, Power’s, Gore’s and Lord Lieutenant’s), and the
+      Guards. Little or nothing changed in terms of the dispersed nature of the
+      companies and troops and the consequent lack of affiliation. Powers
+      Regiment served in France from 1672-74. Two companies of the Guards serves
+      as marines in the abortive amphibious landing turned sea battle at Textel
+      in 1673. There were additional twenty five companies of soldiers sent to
+      join the Guards in 1674. Pay was always months if not years in arrears and
+      most of the officers viewed military service as a part time position.
+      Soldiers were given to furtively taking up a trade to survive and by 1676
+      this was an accepted practice. Command of the Guards went to Lord Ossory
+      in 1674.
+      <br />
+      <br />In 1684, renewed interest in the Irish military situation led to a
+      reestablishment and organization of the Irish Military Establishment under
+      Arthur, Lord Forbes, Earl of Granard and the Marshal of Ireland. There
+      were two regiments of horse (Ormond’s and Arran’s) and a Troop of Life
+      Guards and seven regiments of foot (Ormond’s, Granard’s, Montjoy’s,
+      Newcomen’s, King’s and Fairfax’s) were organized. The Guards continued as
+      one of the Regiments of Foot. That same year, Granard's Regiment was sent
+      to Ulster to disarm potential rebels. In 1685, Granard's Regiment was sent
+      to England in response to Monmouth's Rebellion and returned to Ireland
+      shortly thereafter.
+      <br />
+      <br />As a result of the 1685 purging of Protestants from the Military
+      Establishment of Ireland by the Earl of Tyrone, Arran's Regiment of Horse
+      became Talbot's Regiment of Horse and Ormond's Regiment of Foot went to
+      Justin Mac Carthy. Hamilton’s Regiment of Dragoons was established in
+      1685. Lord Forbes was replaced by Tyrconnel as commander of the Military
+      Establishment and gave command of his Regiment to his son, Arthur Forbes,
+      the second Earl of Granard, who was able to keep a relatively high
+      proportion of Protestants in his regiment. By 1686 of the 7,485 soldiers,
+      5,043 were Catholics and of the 406 officers, 166 were Catholics, with
+      Protestant numbers declining steadily until 1688. MacElligot’s Regiment of
+      Foot was formed in 1688.
+      <br />
+      <br />In 1688, King James II had MacElligot’s Regiment of Foot, A
+      Battalion of the Guards Regiment and Granard's Regiment of Foot
+      transferred to England in preparation for a possible Dutch invasion.
+      Tyrconnel stood up new regiments to replace those sent to England. Of
+      those regiments sent to England, all were disbanded with the exception of
+      Granard's which continued on the English establishment as the 18th
+      regiment of Foot, the Royal Irish Regiment. Granard’s Regiment wore red
+      coats with blue facings and had a red Cross of St Patrick on a white field
+      for their colors. The disbanded Catholic soldiers were to be sent to
+      Austria but many found their way into the service of France. Lord Ossory,
+      by 1688 the 2nd Duke of Ormond, went over to William III and command of
+      the Guards went to William Dorrington. Those Regiments that remained in
+      Ireland in 1688 were effectively purged of Protestants and fought for King
+      James II in the War of Three Kings. When given the chance after the siege
+      of Limerick to stay in Ireland or go into British service, all but seven
+      of the fourteen hundred soldiers of the Royal Regiment of Foot Guards
+      followed General Dorrington into French Service.
+    </p>
+    <hr />
+    <h3>Sources:</h3>
+    <ol>
+      <li>
+        Childs, John, "The Army of Charles II," Routledge and Keegan Paul,
+        London, 1976
+      </li>
+      <li>
+        Childs, John, "The Willaimite Wars in Ireland, 1688-1691", Hambledon
+        Continuum, London, 2007
+      </li>
+      <li>
+        Cannon, Richard, "Historical Record of the Eighteenth or Royal Irish
+        Regiment of Foot" Parker, Furnival and Parker, London, 1848
+      </li>
+      <li>
+        Ellis, Peter, “Hell or Connaught, The Cromwellian Colonization of
+        Ireland, 1652-1660,” Blackstaff Press, Belfast, 1975
+      </li>
+      <li>
+        Falkiner, C. Litton, "The Irish Guards, 1661-1798," Proceedings of the
+        Royal Irish Academy, Vol 24, Dublin, 1904
+      </li>
+
+      <li>
+        Gardiner, Samuel Dawson, “The History of the Commonwealth and
+        Protectorate, 1649-1660.” Vol. III, 1654-1656, Longman Green and Co.,
+        London, 1901
+      </li>
+
+      <li>
+        MacLysaght, Edward, “Irish Life in the Seventeenth Century,” Irish
+        Academic Press, Dublin, 1979
+      </li>
+
+      <li>
+        MacRay, W. Dunn, ed. “Calendar of the Clarendon State Papers”, Vol. III.
+        1655-57 Clarendon Press, Oxford, 1876
+      </li>
+
+      <li>
+        Mahaffey, Robert., “State Papers Relating to Ireland Preserved in the
+        Public Record Office, 1660-62” MacKie and Company, London, 1905
+      </li>
+
+      <li>
+        O’Callaghan, John Cornelius, “History of the Irish Brigades in the
+        Service of France,” Cameron and Ferguson, Glasgow, 1870
+      </li>
+      <li>
+        O’Dowd, Mary, “A History of Women in Ireland, 1500-1800,” Pearson
+        Longman, Harlow, 2005
+      </li>
+      <li>
+        Petrie, Sir Charles, “The Great Tyrconnel, A Chapter in Anglo-Irish
+        Relations,” Mercier Press, Dublin, 1972
+      </li>
+      <li>Tessin List for Great Britain</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+ol {
+  font-size: 80%;
+  color: #444;
+}
+</style>
