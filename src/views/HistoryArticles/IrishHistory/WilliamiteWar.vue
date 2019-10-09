7133018cbc72b0d0e008eaa9c9a35a2114215784
Marichka Offen <marichka.offen@gmail.com>
2019-10-09 16:59:38 -0400

    Work on articles' content

---
+++ src/views/HistoryArticles/IrishHistory/WilliamiteWar.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <h2>Williamite War (1688-1691)</h2>
+    <h3>Order of Battle/Record of Engaged Unit</h3>
+    <div class="image">
+      <img
+        src="@/assets/images/IrishHistory/james-second.jpg"
+        alt="James Second"
+      />
+      <i>
+        <h5>Contemporary Image of the Army of James II</h5>
+      </i>
+    </div>
+    <h3>Army of James II</h3>
+    <h4>Horse</h4>
+    <p>
+      <b>Royal Regiment of Horse</b>
+      <br />
+      <span class="padding">Boyne</span>
+      <br />
+      <span class="padding">Red turned back blue</span>
+      <br />
+      <b>Viscount Galmoy's</b>
+      <br />
+      <span class="padding">Moyry Pass</span>
+      <br />
+      <span class="padding">Limerick</span>
+      <br />
+      <b>Abercorne’s</b>
+      <br />
+      <span class="padding">Aughrim</span>
+      <br />
+      <b>Colclough’s</b>
+      <br />
+      <span class="padding">Limerick (1691)</span>
+      <br />
+      <b>Kilmarnock’s</b>
+      <br />
+      <span class="padding">Limerick (1690)</span>
+
+      <br />
+      <b>Luttrell</b>
+      <br />
+
+      <span class="padding">Enniskillen</span>
+
+      <br />
+      <span class="padding">Sligo (1689)</span>
+
+      <br />
+      <span class="padding">Dublin Garrison (winter 1689)</span>
+
+      <br />
+      <span class="padding">Aughrim</span>
+
+      <br />
+
+      <br />
+      <b>Parker's</b>
+      <br />
+      <span class="padding">Aughrim</span>
+
+      <br />
+      <span class="padding">Pendergast’s</span>
+
+      <br />
+      <span class="padding">Aughrim</span>
+
+      <br />
+      <span class="padding">Purcel</span>
+
+      <br />
+      <span class="padding">Boyne</span>
+
+      <br />
+      <span class="padding">Limerick (1691)</span>
+
+      <br />
+
+      <br />
+      <b>Sarsfield’s</b>
+      <br />
+      <span class="padding">Ballyshannon</span>
+
+      <br />
+      <span class="padding">Sligo (1689)</span>
+
+      <br />
+      <span class="padding">Cavan</span>
+
+      <br />
+      <span class="padding">Boyne</span>
+
+      <br />
+      <span class="padding">Ballineety Raid</span>
+
+      <br />
+      <span class="padding">Castle Birr</span>
+
+      <br />
+      <span class="padding">Aughrim</span>
+
+      <br />
+
+      <br />
+      <b>Sheldon’s</b>
+      <br />
+      <span class="padding">Boyne</span>
+
+      <br />
+      <span class="padding">Aughrim</span>
+
+      <br />
+      <span class="padding">Limerick (1691)</span>
+
+      <br />
+
+      <br />
+      <b>Sunderland's</b>
+      <br />
+      <span class="padding">Boyne</span>
+
+      <br />
+      <span class="padding">Aughrim</span>
+
+      <br />
+
+      <br />
+      <b>Duke of Tyrconnell's</b>
+      <br />
+      <span class="padding">Boyne</span>
+
+      <br />
+
+      <br />
+      <b>Westmeath’s</b>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
