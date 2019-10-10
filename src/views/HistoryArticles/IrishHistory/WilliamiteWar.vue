97bac9184419c66e3fc09457847a8468db81b363
Marichka Offen <marichka.offen@gmail.com>
2019-10-10 16:39:18 -0400

    Work and style Williamite war article Need to add and fix certain text

--- src/views/HistoryArticles/IrishHistory/WilliamiteWar.vue
+++ src/views/HistoryArticles/IrishHistory/WilliamiteWar.vue
@@ -13,128 +13,486 @@
     </div>
     <h3>Army of James II</h3>
     <h4>Horse</h4>
-    <p>
-      <b>Royal Regiment of Horse</b>
-      <br />
-      <span class="padding">Boyne</span>
-      <br />
-      <span class="padding">Red turned back blue</span>
-      <br />
-      <b>Viscount Galmoy's</b>
-      <br />
-      <span class="padding">Moyry Pass</span>
-      <br />
-      <span class="padding">Limerick</span>
-      <br />
-      <b>Abercorne’s</b>
-      <br />
-      <span class="padding">Aughrim</span>
-      <br />
-      <b>Colclough’s</b>
-      <br />
-      <span class="padding">Limerick (1691)</span>
-      <br />
-      <b>Kilmarnock’s</b>
-      <br />
-      <span class="padding">Limerick (1690)</span>
 
-      <br />
-      <b>Luttrell</b>
-      <br />
+    <h5>Royal Regiment of Horse</h5>
+    <span class="padding">Boyne</span>
+    <br />
+    <span class="padding">Red turned back blue</span>
+    <br />
+    <h5>Viscount Galmoy's</h5>
+    <span class="padding">Moyry Pass</span>
+    <br />
+    <span class="padding">Limerick</span>
+    <br />
+    <h5>Abercorne’s</h5>
+    <span class="padding">Aughrim</span>
+    <br />
+    <h5>Colclough’s</h5>
+    <span class="padding">Limerick (1691)</span>
+    <br />
+    <h5>Kilmarnock’s</h5>
+    <span class="padding">Limerick (1690)</span>
 
-      <span class="padding">Enniskillen</span>
+    <br />
+    <h5>Luttrell</h5>
 
-      <br />
-      <span class="padding">Sligo (1689)</span>
+    <span class="padding">Enniskillen</span>
 
-      <br />
-      <span class="padding">Dublin Garrison (winter 1689)</span>
+    <br />
+    <span class="padding">Sligo (1689)</span>
 
-      <br />
-      <span class="padding">Aughrim</span>
+    <br />
+    <span class="padding">Dublin Garrison (winter 1689)</span>
 
-      <br />
+    <br />
+    <span class="padding">Aughrim</span>
 
-      <br />
-      <b>Parker's</b>
-      <br />
-      <span class="padding">Aughrim</span>
+    <br />
+    <h5>Parker's</h5>
+    <span class="padding">Aughrim</span>
 
-      <br />
-      <span class="padding">Pendergast’s</span>
+    <br />
+    <span class="padding">Pendergast’s</span>
 
-      <br />
-      <span class="padding">Aughrim</span>
+    <br />
+    <span class="padding">Aughrim</span>
 
-      <br />
-      <span class="padding">Purcel</span>
+    <br />
+    <span class="padding">Purcel</span>
 
-      <br />
-      <span class="padding">Boyne</span>
+    <br />
+    <span class="padding">Boyne</span>
 
-      <br />
-      <span class="padding">Limerick (1691)</span>
+    <br />
+    <span class="padding">Limerick (1691)</span>
 
-      <br />
+    <br />
+    <h5>Sarsfield’s</h5>
+    <span class="padding">Ballyshannon</span>
 
-      <br />
-      <b>Sarsfield’s</b>
-      <br />
-      <span class="padding">Ballyshannon</span>
+    <br />
+    <span class="padding">Sligo (1689)</span>
 
-      <br />
-      <span class="padding">Sligo (1689)</span>
+    <br />
+    <span class="padding">Cavan</span>
 
-      <br />
-      <span class="padding">Cavan</span>
+    <br />
+    <span class="padding">Boyne</span>
 
-      <br />
-      <span class="padding">Boyne</span>
+    <br />
+    <span class="padding">Ballineety Raid</span>
 
-      <br />
-      <span class="padding">Ballineety Raid</span>
+    <br />
+    <span class="padding">Castle Birr</span>
 
-      <br />
-      <span class="padding">Castle Birr</span>
+    <br />
+    <span class="padding">Aughrim</span>
 
-      <br />
-      <span class="padding">Aughrim</span>
+    <br />
+    <h5>Sheldon’s</h5>
+    <span class="padding">Boyne</span>
 
-      <br />
+    <br />
+    <span class="padding">Aughrim</span>
 
-      <br />
-      <b>Sheldon’s</b>
-      <br />
-      <span class="padding">Boyne</span>
+    <br />
+    <span class="padding">Limerick (1691)</span>
 
-      <br />
-      <span class="padding">Aughrim</span>
+    <br />
+    <h5>Sunderland's</h5>
+    <span class="padding">Boyne</span>
 
-      <br />
-      <span class="padding">Limerick (1691)</span>
+    <br />
+    <span class="padding">Aughrim</span>
 
-      <br />
+    <br />
+    <h5>Duke of Tyrconnell's</h5>
+    <span class="padding">Boyne</span>
 
-      <br />
-      <b>Sunderland's</b>
-      <br />
-      <span class="padding">Boyne</span>
+    <br />
+    <h5>Westmeath’s</h5>
 
-      <br />
-      <span class="padding">Aughrim</span>
+    <h4>Dragoons</h4>
+    <h5>Maxwell’s</h5>
 
-      <br />
+    <span class="padding">Boyne</span>
+    <br />
+    <span class="padding">Limerick (1690)</span>
+    <br />
+    <span class="padding">Althone (1691)</span>
+    <br />
 
-      <br />
-      <b>Duke of Tyrconnell's</b>
-      <br />
-      <span class="padding">Boyne</span>
+    <h5>Lord Clare’s</h5>
 
-      <br />
+    <span class="padding">Derry</span>
+    <br />
+    <span class="padding">Boyne</span>
+    <br />
 
+    <h5>Clifford’s</h5>
+
+    <span class="padding">Limerick (1690)</span>
+    <br />
+    <span class="padding">Limerick (1691)</span>
+    <br />
+
+    <h5>Cotter</h5>
+    <h5>Lord Dungan</h5>
+
+    <span class="padding">Boyne</span>
+
+    <h5>O’Brien</h5>
+
+    <span class="padding">Sligo</span>
+    <br />
+    <span class="padding">Boyne</span>
+    <br />
+
+    <h5>O’Gara’s</h5>
+
+    <span class="padding">Boyne</span>
+
+    <h5>O’Neill’s</h5>
+
+    <span class="padding">Boyne</span>
+    <br />
+    <span class="padding">White turned back brown</span>
+    <br />
+
+    <h5>Talbot’s</h5>
+
+    <span class="padding">Limerick (1690)</span>
+    <h4>Foot</h4>
+    <h5>Royal Regiment of Foot Guards</h5>
+    <span class="padding">Derry</span>
+    <br />
+    <span class="padding">Boyne</span>
+    <br />
+    <span class="padding">Aughrim</span>
+    <br />
+    <span class="padding">Red turned back blue</span>
+    <br />
+    <h5>Earl of Antrim's (Scots/Antrim) (“Redshanks”)</h5>
+    <span class="padding">Derry</span>
+    <br />
+    <span class="padding">Boyne</span>
+    <br />
+    <span class="padding">Althone</span>
+    <br />
+    <span class="padding">Aughrim</span>
+    <br />
+    <span class="padding">Limerick</span>
+    <br />
+    <span class="padding">White turned back red</span>
+    <br />
+    <h5>Barret’s</h5>
+    <span class="padding">Waterford Garrison</span>
+    <br />
+    <h5>Bagenal’s</h5>
+    <span class="padding">Derry</span>
+    <br />
+    <h4>Lord Bellew’s</h4>
+    <br />
+    <br />Text with no style! Need help
+    <br />
+    <br />
+    <div class="image">
+      <img
+        src="@/assets/images/IrishHistory/enniskillen.jpg"
+        alt="Raising the Enniskillen Regiment"
+      />
+      <i>
+        <h5>Raising the Enniskillen Regiment</h5>
+      </i>
+    </div>
+    <h4>Battles and Sieges</h4>
+    2 March 1689 Justin McCarthy occupies Bandon
+    <br />
+    <br />12 March 1689 King James II lands at Kinsale
+    <br />
+    <br />December - August 1689 Actions around Enniskillen by Enniskillen
+    forces
+    <br />
+    <br />18 December 2 companies established under Cpts. Allen and Malcome
+    Cathcart
+    <br />
+    <br />January Enniskillen Regt formed under Col. Gustavus Hamilton and LTC
+    Lloyd
+    <br />
+    <br />20-24 Mar Siege of Crom Castle
+    <br />
+    <br />24 Apr Raid on Omagh
+    <br />
+    <span class="padding">Burning of Augher</span>
+    <br />
+    <span class="padding">Raid on Monaghan</span>
+    <br />
+    <br />4 May Belleek
+    <br />
+    <br />Late May Redhill
+    <br />
+    <span class="padding">Ballynacarrig</span>
+    <br />
+    <span class="padding">Kells</span>
+    <br />
+    <span class="padding">Finna</span>
+    <br />
+    <br />3 June Raid on Omagh
+    <br />
+    <br />11 June Raid on Omagh
+    <br />
+    <br />17 June Belturbet
+    <br />
+    <br />8 July Cornegrade
+    <br />
+    <br />20 July Enniskillen organizes into Wolsely’s Horse, Wynne’s Dragoons,
+    Hamilton’s Foot, Lloyd’s Foot and Tiffins’s Foot
+    <br />
+    <br />31 July Crom Castle
+    <br />
+    <span class="padding">Lisnaskea</span>
+    <br />
+    <span class="padding">Newtown-Butler</span>
+    <br />
+    <br />18 April - 1 August 1689 Siege of Derry
+    <h4>Actions around Derry by Regiments in Derry</h4>
+    14 March 1689 Dromore
+    <br />
+    <br />27 March 1689 Coleriane
+    <br />
+    <br />14 April 1689 Cladyford
+    <br />
+    <br />21 April Pennyburn Mill
+    <br />
+    <br />5 May Windmill Hill
+    <br />
+    <br />28 June Butchers Gate at Derry
+    <br />
+    <br />4 June Windmill Hill
+    <br />
+    <br />May 1689 Ballyshannon
+    <br />
+    <br />1 May- 30 Sep 1689 operations around Sligo
+    <br />
+    <br />May 1689 – 14 June 1690 Siege of Charlemont
+    <br />
+    <br />13 August 1689 Schomberg lands at Bangor Bay
+    <br />
+    <br />20-27 August 1689 Siege of Carrickfergus
+    <br />
+    <br />21 September 1689 Dundalk
+    <br />
+    <br />January 1690 Siege of Kenagh Castle
+    <br />
+    <br />7 June 1690 Ballymore Fort
+    <br />
+    <br />14 June 1690 William III lands at Carrickfergus
+    <br />
+    <br />22 June 1690 Skirmish at Moyry Pass
+    <br />
+    <br />22 June 1690 Newry
+    <br />
+    <br />1 July 1690 Battle of the Boyne
+    <br />
+    <br />17-24 July 1690 Siege of Althone
+    <br />
+    <br />21-26 July 1690 Siege of Duncannon Fort
+    <br />
+    <br />23-24 July 1690 Siege of Waterford
+    <br />
+    <br />9-30 August 1690 Siege of Limerick
+    <br />
+    <br />13 August 1690 Ballineety Raid
+    <br />
+    <br />Sep 1690 Althone
+    <br />
+    <br />12 September 1690 Castle Birr
+    <br />
+    <br />28 September 1690 Cork
+    <br />
+    <br />3 October 1690 Kinsale
+    <br />
+    <br />December 1690 Jamestown
+    <br />
+    <br />27 December 1690 - 8 January 1691 Lanesborough
+    <br />
+    <br />21-30 June 1691 Siege of Althone
+    <br />
+    <br />June 1691 Ballymore Fort
+    <br />
+    <br />12 July 1691 Aughrim
+    <br />
+    <br />19- 28 July 1691 Galway
+    <br />
+    <br />25 August – 3 October 1691 Siege of Limerick
+    <br />
+    <br />10 - 15 September 1691 Seige of Sligo
+    <div class="image">
+      <img
+        src="@/assets/images/IrishHistory/boyne.jpg"
+        alt="Battle of the Boyne"
+      />
+      <i>
+        <h5>Battle of the Boyne</h5>
+      </i>
+    </div>
+    <i>
+      <h4>The Boyne Water</h4>
+      <br />July the first, of a morning clear, one thousand six hundred and
+      ninety, <br />King William did his men prepare—of thousands he had thirty-
+      <br />To fight King James and all his foes, encamped near the Boyne Water;
+      <br />He little feared, though two to one, their multitude to scatter.
       <br />
-      <b>Westmeath’s</b>
-    </p>
+      <br />King William called his officers, saying: "Gentlemen, mind your
+      station, <br />And let your valour here be shown before this Irish nation;
+      <br />My brazen walls let no man break, and your subtle foes you’ll
+      scatter, <br />Be sure you show them good English play as you go over the
+      water."
+      <br />
+      <br />Both foot and horse they marched on, intending them to batter,
+      <br />But the brave Duke Schomberg he was shot as he crossed over the
+      water. <br />When that King William did observe the brave Duke Schomberg
+      falling, <br />He reined his horse with a heavy heart, on the
+      Enniskillenes calling:
+      <br />
+      <br />"What will you do for me, brave boys—see yonder men retreating?
+      <br />Our enemies encouraged are, and English drums are beating." <br />He
+      says, "My boys feel no dismay at the losing of one commander, <br />For
+      God shall be our King this day, and I'll be general under."
+      <br />
+      <br />Within four yards of our fore-front, before a shot was fired,
+      <br />A sudden snuff they got that day, which little they desired;
+      <br />For horse and man fell to the ground, and some hung on their saddle:
+      <br />Others turned up their forked ends, which we call coup de ladle.
+      <br />
+      <br />Prince Eugene's regiment was the next, on our right hand advanced
+      <br />Into a field of standing wheat, where Irish horses pranced;
+      <br />But the brandy ran so in their heads, their senses all did scatter,
+      <br />They little thought to leave their bones that day at the Boyne
+      Water.
+      <br />
+      <br />Both men and horse lay on the ground, and many there lay bleeding,
+      <br />I saw no sickles there that day—but, sure, there was sharp shearing.
+      <br />Now, praise God, all true Protestants, and heaven's and earth's
+      Creator, <br />For the deliverance he sent our enemies to scatter.
+      <br />The Church's foes will pine away, like churlish-hearted Nabal,
+      <br />For our deliverer came this day like the great Zorobabal.
+      <br />
+      <br />So praise God, all true Protestants, and I will say no further,
+      <br />But had the Papists gained that day, there would have been open
+      murder. <br />Although King James and many more were ne'er that way
+      inclined, <br />It was not in their power to stop what the rabble they
+      designed.
+    </i>
+    <div class="image">
+      <img
+        src="@/assets/images/IrishHistory/wick.jpeg"
+        alt="Jan Wyck’s Painting of the Battle of the Boyne"
+      />
+      <i>
+        <h5>Jan Wyck’s Painting of the Battle of the Boyne</h5>
+      </i>
+    </div>
+    <i>
+      <h4>Lillibulero</h4>
+
+      <br />Ho! brother Teague, dost hear the decree? <br />Lillibulero bullen a
+      la, (repeated after each line) <br />Dat we shall have a new deputy?
+      <br />Lillibulero bullen a la.
+      <br />
+      <br />Refrain: <br />Lero, lero, lillibulero, <br />lillibulero, bullen a
+      la <br />Lero, lero, lillibulero, <br />lillibulero, bullen a la.
+      <br />
+      <br />Ho! by my soul, it is de Talbot, <br />And he will cut all de
+      English throat.
+      <br />
+      <br />Though, by my soul, de English do praat, <br />De law's on dare
+      side, and Creish knows what.
+      <br />
+      <br />But if dispence do come from de Pope, <br />We'll hang Magna Charta
+      and dem in a rope.
+      <br />
+      <br />And de good Talbot is made a Lord, <br />And with brave lads is
+      coming abroad.
+      <br />
+      <br />Who all in France have taken a sware, <br />Dat dey will have no
+      Protestant heir.
+      <br />
+      <br />Ara! but why does he stay behind? <br />Ho' by my soul, 'tis a
+      Protestant wind!
+      <br />
+      <br />But see, de Tyrconnel is now come ashore, <br />And we shall have
+      commissions galore.
+      <br />
+      <br />And he dat will not go to mass <br />Shall be turn out, and look
+      like an ass.
+      <br />
+      <br />But now de hereticks all go down, <br />By Creish and St. Patrick,
+      de nation's our own.
+      <br />
+      <br />Dare was an old prophecy found in a bog, <br />" Ireland shall be
+      ruled by an ass and a dog".
+      <br />
+      <br />And now dis prophecy is come to pass, <br />For Talbot's de dog, and
+      James is de ass.
+    </i>
+    <hr />
+    <h3>Sources:</h3>
+    <ol>
+      <li>
+        Doherty, Richard, The Williamite War in Ireland, 1688-1691 ( Ireland,
+        1998)
+      </li>
+      <li>
+        Ede-Borrett, S., The Army of James II, Uniforms and Organization ( UK,
+        2003)
+      </li>
+      <li>
+        Berresford-Ellis, Peter, The Boyne Water, The Battle of the Boyne, 1690
+        ( Belfast, 1989)
+      </li>
+      <li>
+        Glozier, Matthew, The Huguenot Soldiers of William of Orange and the
+        “Glorious Revolution” of 1688 ( UK, 2002)
+      </li>
+      <li>
+        Hamilton, Andrew, The Actions of the Enniskillen-Men ( London, 1690)
+      </li>
+      <li>
+        Kinross, John, The Boyne and Aughrim, The War of Two Kings ( UK, 1997)
+      </li>
+      <li>Lenihan, Padraig, 1690: Battle of the Boyne ( UK, 2003)</li>
+      <li>Macrory, Patrick, The Siege of Derry (NY 1980)</li>
+      <li>McNally Michael, Battle of the Boyne, 1690 ( UK, 2005)</li>
+      <li>Reid, Stuart, The Last Scots Army, 1661-1714 (UK 2003)</li>
+      <li>Sapherson, C.A., The British Army of William III ( UK, 1997)</li>
+      <li>
+        Sapherson, C.A., William III At War Scotland and Ireland 1689-1691 ( UK,
+        2001)
+      </li>
+      <li>Simms, J.G., Jacobite Ireland ( Dublin, 2000)</li>
+      <li>
+        Wauchope, Piers, Patrick Sarsfield and the Williamite War ( Dublin,
+        1992)
+      </li>
+      <li>
+        Williams, Noel St John, Redcoats and Courtesans, the Birth of the
+        British Army, 1660-1690 ( London, 1994)
+      </li>
+      <li>
+        Witherow, Thomas, Derry and Enniskillen in the Year 1689 ( Belfast,
+        1913)
+      </li>
+      <li>
+        A Jacobite narrative of the war in Ireland , A light to the blind,
+        Plunkett memoirs
+      </li>
+      <li>A List of Officers taken at Ballymore, the 8th of June, 1691</li>
+      <li>
+        Contemporary Diary of Siege of Limerick, 1691, by Colonel Michael
+        Richards
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -142,4 +500,8 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h4 {
+  margin-bottom: 0;
+}
+</style>
